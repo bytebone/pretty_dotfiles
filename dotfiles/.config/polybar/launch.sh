24722a818b043ec41b7ce5f73ef5dce95b079ba8
justrainer <>
2023-04-28 20:55:04 +0000

    Updated from private repo

---
+++ dotfiles/.config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+# polybar top -c ~/.config/polybar/config.ini &
+
+for m in $(polybar -m | cut -d":" -f1);
+do
+    MONITOR=$m polybar top-bar &
+done
